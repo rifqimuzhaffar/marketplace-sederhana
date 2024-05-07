7802e23b65bc3556dbd1533af8ed58495d91388c
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-05-08 05:35:51 +0700

    remake structur folder productpage

--- src/components/Elements/CardProducts/index.jsx
+++ src/components/Elements/CardProducts/index.jsx
@@ -1,65 +1,10 @@
 import { FiShoppingCart } from "react-icons/fi";
 
-const products = [
-  {
-    id: 1,
-    name: "-- Espresso --",
-    image: "/images/espresso.jpg",
-    alt: "Espresso",
-    price: "IDR 15K",
-  },
-  {
-    id: 2,
-    name: "-- Cappuccino --",
-    image: "/images/cappuccino.jpg",
-    alt: "Cappuccino",
-    price: "IDR 20K",
-  },
-  {
-    id: 3,
-    name: "-- Espresso Romano --",
-    image: "/images/Espresso-Romano.jpg",
-    alt: "Espresso Romano",
-    price: "IDR 18K",
-  },
-  {
-    id: 4,
-    name: "-- Vanilla Latte --",
-    image: "/images/vanilla-latte.jpg",
-    alt: "Vanilla Latte",
-    price: "IDR 22K",
-  },
-  {
-    id: 5,
-    name: "-- Sweet Mocha--",
-    image: "/images/Sweet-Mocha.jpg",
-    alt: "Sweet Mocha",
-    price: "IDR 21K",
-  },
-  {
-    id: 6,
-    name: "-- Caramel --",
-    image: "/images/caramel.jpg",
-    alt: "Caramel",
-    price: "IDR 25K",
-  },
-];
-
 const CardProducts = (props) => {
   const { children } = props;
   return (
-    <div className="flex flex-wrap mt-[1.5rem] justify-center gap-6 md:px-[5px] lg:px-[11rem]">
-      {products.map((product) => (
-        <div
-          key={product.id}
-          className="text-center drop-shadow-md bg-black/80 px-6 py-4 rounded-xl flex flex-col justify-between gap-2"
-        >
-          {children}
-          <CardProducts.Header image={product.image} alt={product.alt} />
-          <CardProducts.Body name={product.name} />
-          <CardProducts.Footer price={product.price} />
-        </div>
-      ))}
+    <div className="text-center drop-shadow-md bg-black/80 px-6 py-4 rounded-xl flex flex-col justify-between gap-2">
+      {children}
     </div>
   );
 };
@@ -78,7 +23,7 @@ const Footer = (props) => {
   const { price } = props;
   return (
     <div className="flex justify-between items-center">
-      <p>{price}</p>
+      <p>IDR 15000</p>
       <a className="border p-2 rounded-full bg-black hover:bg-white">
         <FiShoppingCart className="h-5 w-5 text-primary" />
       </a>
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -2,6 +2,51 @@ import TopNavbar from "../components/Elements/topnavbar";
 import Tittle from "../components/Elements/Tittle";
 import CardProducts from "../components/Elements/CardProducts";
 
+const products = [
+  {
+    id: 1,
+    name: "-- Espresso --",
+    image: "/images/espresso.jpg",
+    alt: "Espresso",
+    price: 15000,
+  },
+  {
+    id: 2,
+    name: "-- Cappuccino --",
+    image: "/images/cappuccino.jpg",
+    alt: "Cappuccino",
+    price: 20000,
+  },
+  {
+    id: 3,
+    name: "-- Espresso Romano --",
+    image: "/images/Espresso-Romano.jpg",
+    alt: "Espresso Romano",
+    price: 18000,
+  },
+  {
+    id: 4,
+    name: "-- Vanilla Latte --",
+    image: "/images/vanilla-latte.jpg",
+    alt: "Vanilla Latte",
+    price: 22000,
+  },
+  {
+    id: 5,
+    name: "-- Sweet Mocha--",
+    image: "/images/Sweet-Mocha.jpg",
+    alt: "Sweet Mocha",
+    price: 21000,
+  },
+  {
+    id: 6,
+    name: "-- Caramel --",
+    image: "/images/caramel.jpg",
+    alt: "Caramel",
+    price: 25000,
+  },
+];
+
 const Products = () => {
   return (
     <div>
@@ -9,7 +54,15 @@ const Products = () => {
       <section className="bg-black bg-homepage min-h-screen text-white bg-cover object-cover bg-no-repeat bg-bottom flex pb-10">
         <div className="mx-auto">
           <Tittle />
-          <CardProducts />
+          <div className="flex flex-wrap mt-[1.5rem] justify-center gap-6 md:px-[5px] lg:px-[11rem]">
+            {products.map((product) => (
+              <CardProducts key={product.id}>
+                <CardProducts.Header image={product.image} alt={product.alt} />
+                <CardProducts.Body name={product.name} />
+                <CardProducts.Footer price={product.price} />
+              </CardProducts>
+            ))}
+          </div>
         </div>
       </section>
     </div>
