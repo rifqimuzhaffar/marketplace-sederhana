342c5aa045ca106a704e9e0620bdd9c67229078a
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-07-13 14:23:33 +0700

    -

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,9 @@ import Product from "./pages/product.jsx";
 import About from "./pages/about.jsx";
 import Contact from "./pages/contact.jsx";
 import ErrorPage from "./pages/404.jsx";
-<<<<<<< HEAD
-import Admin from "./pages/admin/admin.jsx";
-import Dashboard from "./pages/admin/dashboard.jsx";
-import AdminProduct from "./pages/admin/product.jsx";
-import Order from "./pages/admin/order.jsx";
-=======
 import Login from "./pages/login.jsx";
 import Register from "./pages/register.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
->>>>>>> 9ab2d963291abfb373ddf44822872c07833e152b
 
 const router = createBrowserRouter([
   {
@@ -37,31 +30,12 @@ const router = createBrowserRouter([
     element: <Contact />,
   },
   {
-<<<<<<< HEAD
-    path: "/admin",
-    element: <Admin />,
-    children: [
-      {
-        path: "dashboard",
-        element: <Dashboard />,
-      },
-      {
-        path: "product",
-        element: <AdminProduct />,
-      },
-      {
-        path: "order",
-        element: <Order />,
-      },
-    ],
-=======
     path: "/login",
     element: <Login />,
   },
   {
     path: "/register",
     element: <Register />,
->>>>>>> 9ab2d963291abfb373ddf44822872c07833e152b
   },
 ]);
 
